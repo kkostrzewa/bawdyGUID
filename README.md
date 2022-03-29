67939f184296e815836358a1f593986046d69bb2
Kevin Kostrzewa <kevin.kostrzewa@gmail.com>
2022-03-29 18:57:56 -0400

    add progress bar & speed
    - refactored algorithm for speed
    - add a progress bar
    - write to first argv filename instead of stdout
    - filter out uninteresting output

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # bawdyGUID
 Python script to generate GUIDs with words, looking for bawdy text. Also an excuse to kick the tires on python. An uninterrupted run is **s** to the **l** to the **o** to the **w**.
 
+## installing
+- python 3
+- `pip install english_words alive_progress`
+
+## running
+- `bawdyGUID.py [OUTFILE]`
+
 ## guts
 Recurse through an english dictionary and add words along with 1337 substitutions. 1337 substitutions are iterated through and every combination is applied (see *guidify*). *processGuidSentence* receives each GUID 'sentence' and outputs a dict of the sentence and attributes (are our words aligned on GUID dashes, etc.), so subjective judgements can be made (would a screen cap of that be a dict pic?).
 
 ```mermaid
 flowchart TD
-    Start --> addWords
+    Start --> guidify
+    guidify --> lcGuidifiedWords
+    lcGuidifiedWords --> addWords
     addWords --> addWords
-    addWords --> guidify
     addWords --> processGuidSentence
 ```
 
--- bawdyGUID.py
+++ bawdyGUID.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+import sys
 import re
 from urllib.request import urlopen
 from english_words import english_words_lower_alpha_set
+from alive_progress import alive_bar
 
-# read the cursed words
-badWords = {ii.decode('utf-8').strip() for ii in urlopen("https://www.cs.cmu.edu/~biglou/resources/bad-words.txt")}
+if len(sys.argv) != 2:
+    print(f"USAGE: {sys.argv[0]} [OUTFILE]")
+    raise Exception('bad arguments')
+outfileName = sys.argv[1]
 
-lcEnglishWordsWithCurses = badWords | english_words_lower_alpha_set
+maxProgressBarDepth = 6
 
 # 1337 subs, from https://nedbatchelder.com/text/hexwords.html
 sub1337 = [
@@ -27,6 +31,9 @@ sub1337 = [
 isHexCharsRegex = re.compile('^[0-9a-f]+$')
 guidGenRegex = re.compile('^([0-9a-f]{0,8})([0-9a-f]{0,4})([0-9a-f]{0,4})([0-9a-f]{0,4})([0-9a-f]{0,12})$')
 
+# read the cursed words
+badWords = {ii.decode('utf-8').strip() for ii in urlopen("https://www.cs.cmu.edu/~biglou/resources/bad-words.txt")}
+
 # return a set of all GUID compliant ([0-9a-f]) 1337 words for argument *word*
 def guidify(word):
     ret = set()
@@ -45,29 +52,45 @@ def guidify(word):
             ret |= { subWord }
     return ret
 
+# build our set of guidified words
+with alive_bar(len(badWords | english_words_lower_alpha_set)) as progressBar:
+    progressBar.text('Cultivating impropriety ...')
+    lcGuidifiedWords = []
+    for word in (badWords | english_words_lower_alpha_set| english_words_lower_alpha_set):
+        progressBar()
+        g = guidify(word)
+        if (g):
+            lcGuidifiedWords.append({'word':word, 'guidWords':g})
+
+print(f"guidified words len {len(lcGuidifiedWords)}, guidified bad words len {len([ii for ii in lcGuidifiedWords if ii['word'] in badWords])}");
+
 # add guid words to cwords collection, as long as there is lenAvail space to add
 # recurse like hell
 # return true if we've added words to the collection, so we get the biggest collection 
 # possible
-def addWords(cwords, lenAvail):
+def addWords(outfile, cwords, lenAvail, recursionLevel, progressBar):
     # donezo test
     if lenAvail <= 0:
         return False
 
+    # advance the progress bar 
+    if (recursionLevel <= maxProgressBarDepth):
+        progressBar()
+
     wordsAdded = False
-    for word in lcEnglishWordsWithCurses:
-        for gword in guidify(word):
+    for ii in lcGuidifiedWords:
+        for gword in ii['guidWords']:
             if len(gword) <= lenAvail:
                 wordsAdded = True
-                cwordsAdd = cwords + [{'word':word, 'gword':gword}]
+                cwordsAdd = cwords + [{'word':ii['word'], 'gword':gword}]
                 # try to add words. if we can't add any more, "process" them
-                if not addWords(cwordsAdd, lenAvail - len(gword)):
-                    processGuidSentence(cwordsAdd)
+                if not addWords(outfile, cwordsAdd, lenAvail - len(gword), recursionLevel+1, progressBar):
+                    processGuidSentence(outfile, cwordsAdd)
     
     return wordsAdded
 
 # a GUID word was found. process it.
-def processGuidSentence(cwords):
+def processGuidSentence(outfile, cwords):
     # are gword boundries aligned with GUID -'s (found at positions 8, 12, 16, 20)?
     bpos = 0    # word break position
     guidDashPos = [8, 12, 16, 20]   # GUID dash positions
@@ -83,6 +106,13 @@ def processGuidSentence(cwords):
             guidDashPos.pop(0)
     dashAligned = len(guidDashPos) == 0
 
+    # filter out uninteresting data
+    if not hasBadWords:
+        return
+    # no repeating words. so boring
+    if len([v for i, v in enumerate(cwords) if i == 0 or v != cwords[i - 1]]) != len(cwords):
+        return
+
     # generate GUID and some attributes about GUID in a dict
     m = guidGenRegex.match(''.join([ii['gword'] for ii in cwords]))
     out = {
@@ -93,7 +123,9 @@ def processGuidSentence(cwords):
         'dashAligned': dashAligned,
         'hasBadWords': hasBadWords,
     }
-    print(out)
+    outfile.write(str(out)+'\n')
 
 # start the recursive process
-addWords([], 32)
+with open(outfileName, 'w') as outfile, alive_bar(len(lcGuidifiedWords)**maxProgressBarDepth) as progressBar:
+    progressBar.text('Fomenting ribaldry...')
+    addWords(outfile, [], 32, 0, progressBar)
